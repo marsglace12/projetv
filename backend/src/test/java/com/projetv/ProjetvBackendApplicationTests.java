be0136ae9cd1e397c20bf8702ff9b45fa6bffe5f
Jeremy Martin <jeremymartinperso@gmail.com>
2026-01-01 21:03:57 +0100

    feat(backend): Init spring boot project and add healthcheck

---
+++ backend/src/test/java/com/projetv/ProjetvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
